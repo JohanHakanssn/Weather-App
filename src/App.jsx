67503cbf4b98120fd20c2949a32f1ff548639e27
Johan Håkansson <johan1hakansson@gmail.com>
2024-07-31 13:47:09 +0200

    mfc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './assets/styles/global.css';
+import { fetchWeatherData } from './api/weatherAPI';
 
 function App() {
 	return (
@@ -8,29 +9,6 @@ function App() {
 		</div>
 	);
 }
-
-// API Key and base URL
-const apiKey = 'ZBAD2VNSZPH8ZAXP3VV94JEXQ';
-const baseUrl =
-	'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
-
-// Funktion för att hämta data från API:et
-const fetchWeatherData = async (location) => {
-	const url = `${baseUrl}/${location}?aggregateHours=24&unitGroup=metric&shortColumnNames=false&contentType=json&key=${apiKey}`;
-
-	try {
-		const response = await fetch(url, { mode: 'cors' });
-		if (!response.ok) {
-			throw new Error(`Response status: ${response.status}`);
-		}
-		const json = await response.json();
-		console.log('Fetched JSON:', json); // Log the entire JSON response for debugging
-		return json;
-	} catch (error) {
-		console.error('Error fetching weather data:', error.message);
-		return null;
-	}
-};
 
 export default App;
 
@@ -51,7 +29,7 @@ function FetchData() {
 
 		if (data) {
 			const selectedData = {
-				address: data.address,
+				address: data.resolvedAddress,
 				description: data.description,
 				temp: data.currentConditions.temp,
 				conditions: data.currentConditions.conditions,
@@ -79,7 +57,7 @@ function FetchData() {
 			</form>
 			{weatherData && (
 				<div>
-					<h2>Weather for {location}</h2>
+					<h2>Weather for {weatherData.address}</h2>
 					<p>{weatherData.description}</p>
 					<p>Temperature: {weatherData.temp} °C</p>
 					<p>Conditions: {weatherData.conditions}</p>
---
+++ src/api/weatherAPI.jsx
@@ -0,0 +1,22 @@
+// API Key and base URL
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const baseUrl =
+	'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
+
+// Funktion för att hämta data från API:et
+export const fetchWeatherData = async (location) => {
+	const url = `${baseUrl}/${location}?aggregateHours=24&unitGroup=metric&shortColumnNames=false&contentType=json&key=${apiKey}`;
+
+	try {
+		const response = await fetch(url, { mode: 'cors' });
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+		const json = await response.json();
+		console.log('Fetched JSON:', json); // Log the entire JSON response for debugging
+		return json;
+	} catch (error) {
+		console.error('Error fetching weather data:', error.message);
+		return null;
+	}
+};
